911ce396307b7a368a5470c3aff7621f11a74be7
Eaton <AndrewEaton@PSNL.CA>
2017-01-17 09:30:32 -0330

    working code

--- script.js
+++ script.js
@@ -43,6 +43,7 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 // 'speak' method.
 // See Lecture 50, part 1
 for (var i = 0; i < names.length; i++) {
+  //console.log(names[i])
 
   // STEP 11: (NOTHING TO DO. ALREADY DONE FOR YOU)
   // Retrieve the first letter of the current name in the loop.
@@ -59,9 +60,13 @@ for (var i = 0; i < names.length; i++) {
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
   if (firstLetter === 'j') {
-    byeSpeaker.speak(names[i]);
+  byeSpeaker.speak(names[i]);
+ 
+    
   } else {
-    helloSpeaker.speak(names[i]);
+  helloSpeaker.speak(names[i]);
+  
+    
   }
 }
 
